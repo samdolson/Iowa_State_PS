a6b414440bfb87904e8900f00fec98c950522879
samdolson <samuel.david.olson@gmail.com>
2024-11-14 20:53:20 -0600

    R HW tidying

--- STAT 5790/Notes/testDb.R
+++ STAT 5790/Notes/testDb.R
@@ -6,6 +6,8 @@ DBI::dbListTables(con)
 
 DBI::dbListFields(con, "ecom")
 
+DBI::dbGetQuery(conn = con, statement = "PRAGMA table_info(ecom)")
+
 DBI::dbReadTable(conn = con, name = 'COMPANY')
 
 DBI::dbGetQuery(conn = con, statement = "select * from ecom limit 20")
